e80ec5bdcc23c6c9fb7d1a1279e94f7019f0afed
Vitaliylevchuk <vitalik.levchuk.008@gmail.com>
2024-10-01 13:24:46 +0300

    clear test bot and overrides new methods

--- src/main/java/org/fbs/stgbot/bot/TestEchoBot.java
+++ src/main/java/org/fbs/stgbot/bot/TestEchoBot.java
@@ -1,49 +1,42 @@
 package org.fbs.stgbot.bot;
 
+import com.pengrad.telegrambot.model.*;
 import org.fbs.stgbot.bot.base.Bot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public final class TestEchoBot extends Bot {
-    public TestEchoBot() throws TelegramApiException {
-        super("5792337403:AAFxy4ksALgHyCw-2pNzcG1jFPaHHmbABMI");
+
+
+    public TestEchoBot(String botToken, String startCommandRaw) {
+        super(botToken, startCommandRaw);
     }
 
     @Override
-    public String getBotUsername() {
-        return "Vil_Console_bot";
-    }
-
-    @Override
-    protected void onStartCommand(Message message) throws TelegramApiException {
-        // Set the chat id of the message
-        SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(message.getChatId());
-        sendMessage.setText("Start command is received");
-
-        execute(sendMessage);
-    }
-
-    @Override
-    protected void updateParse(Update update) throws TelegramApiException {
+    protected void onStartCommand(Message message) {
 
     }
 
     @Override
-    protected void callbackQueryParse(Update update) throws TelegramApiException {
+    protected void updateParse(Update update) {
 
     }
 
     @Override
-    protected void messageParse(Update update) throws TelegramApiException {
+    protected void callbackQueryParse(CallbackQuery query) {
 
     }
 
     @Override
-    protected void inlineQueryParse(Update update) throws TelegramApiException {
+    protected void entitiesParse(MessageEntity[] messageEntities) {
 
     }
 
+    @Override
+    protected void messageParse(Message message) {
+
+    }
+
+    @Override
+    protected void inlineQueryParse(InlineQuery query) {
+
+    }
 }
