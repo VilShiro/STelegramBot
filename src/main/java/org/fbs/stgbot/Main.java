7c7dc9e71e3144c68b8ed6aa7533d412a4a723e5
Vitaliylevchuk <vitalik.levchuk.008@gmail.com>
2024-10-01 13:38:04 +0300

    Some edits in Main class

--- src/main/java/org/fbs/stgbot/Main.java
+++ src/main/java/org/fbs/stgbot/Main.java
@@ -1,12 +1,12 @@
 package org.fbs.stgbot;
 
-import org.fbs.stgbot.bot.TestEchoBot;
+import org.fbs.stgbot.bot.TestBot;
 
 public class Main {
 
 
     public static void main(String[] args){
-        TestEchoBot echoBot = new TestEchoBot("8187803651:AAGVXbuZGcPXXBaP3rd823fYDjm-uYg1-aU", "start");
+        TestBot echoBot = new TestBot("8187803651:AAGVXbuZGcPXXBaP3rd823fYDjm-uYg1-aU", "start");
 
     }
 
