396c635c29aae885df23c6353aabb9f678341235
Vitaliy <vitalik.levchuk.008@gmail.com>
2024-10-03 20:47:11 +0300

    Adding exceptions for parsing methods, adding a deprecation annotations

--- src/main/java/org/fbs/stgbot/bot/base/MultiClientBot.java
+++ src/main/java/org/fbs/stgbot/bot/base/MultiClientBot.java
@@ -3,7 +3,6 @@ package org.fbs.stgbot.bot.base;
 import com.pengrad.telegrambot.model.*;
 import org.fbs.stgbot.data.ClientThreads;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 public abstract class MultiClientBot extends Bot {
@@ -19,7 +18,7 @@ public abstract class MultiClientBot extends Bot {
         super(botToken, startCommandRaw);
     }
 
-    protected ClientThreads getThreadsById(long userId, ArrayList<ClientThreads> threadsArrayList){
+    protected ClientThreads getThreadsByUserId(long userId, ArrayList<ClientThreads> threadsArrayList){
         for (ClientThreads threads : threadsArrayList) {
             if (threads.getUserId() == userId) {
                 return threads;
@@ -28,69 +27,81 @@ public abstract class MultiClientBot extends Bot {
         return null;
     }
 
-    protected abstract void callbackQueryParse(CallbackQuery query, ArrayList<ClientThreads> threads);
+    protected abstract void callbackQueryParse(CallbackQuery query, ArrayList<ClientThreads> threads) throws Exception;
 
-    protected abstract void entitiesParse(MessageEntity[] messageEntities, Message message, ArrayList<ClientThreads> threads);
+    protected abstract void entitiesParse(MessageEntity[] messageEntities, Message message, ArrayList<ClientThreads> threads) throws Exception;
 
-    protected abstract void inlineQueryParse(InlineQuery query, ArrayList<ClientThreads> threads) throws IOException;
+    protected abstract void inlineQueryParse(InlineQuery query, ArrayList<ClientThreads> threads) throws Exception;
 
-    protected abstract void messageParse(Message message, ArrayList<ClientThreads> threads);
+    protected abstract void messageParse(Message message, ArrayList<ClientThreads> threads) throws Exception;
 
-    protected abstract void onStartCommand(Message message, ArrayList<ClientThreads> threads);
+    protected abstract void onStartCommand(Message message, ArrayList<ClientThreads> threads) throws Exception;
 
-    protected abstract void updateParse(Update update, ArrayList<ClientThreads> threads);
+    protected abstract void updateParse(Update update, ArrayList<ClientThreads> threads) throws Exception;
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsCallbackBack() {
         return parsingThreadsCallbackBack;
     }
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsEntities() {
         return parsingThreadsEntities;
     }
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsInline() {
         return parsingThreadsInline;
     }
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsMessages() {
         return parsingThreadsMessages;
     }
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsStart() {
         return parsingThreadsStart;
     }
 
+    @Deprecated
     public ArrayList<ClientThreads> getParsingThreadsUpdates() {
         return parsingThreadsUpdates;
     }
 
+    @Deprecated
     @Override
-    protected void callbackQueryParse(CallbackQuery query) {
-
+    protected void callbackQueryParse(CallbackQuery query) throws Exception {
+        callbackQueryParse(query, parsingThreadsCallbackBack);
     }
 
+    @Deprecated
     @Override
-    protected void entitiesParse(MessageEntity[] messageEntities, Message message) {
-
+    protected void entitiesParse(MessageEntity[] messageEntities, Message message) throws Exception {
+        entitiesParse(messageEntities, message, parsingThreadsEntities);
     }
 
+    @Deprecated
     @Override
-    protected void inlineQueryParse(InlineQuery query) throws IOException {
-
+    protected void inlineQueryParse(InlineQuery query) throws Exception {
+        inlineQueryParse(query, parsingThreadsInline);
     }
 
+    @Deprecated
     @Override
-    protected void messageParse(Message message) {
-
+    protected void messageParse(Message message) throws Exception {
+        messageParse(message, parsingThreadsMessages);
     }
 
+    @Deprecated
     @Override
-    protected void onStartCommand(Message message) {
-
+    protected void onStartCommand(Message message) throws Exception {
+        onStartCommand(message, parsingThreadsStart);
     }
 
+    @Deprecated
     @Override
-    protected void updateParse(Update update) {
-
+    protected void updateParse(Update update) throws Exception {
+        updateParse(update, parsingThreadsUpdates);
     }
 }
