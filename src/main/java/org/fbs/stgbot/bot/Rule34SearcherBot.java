49c1fb0115e4ca35e1646101c35b9a978d402f0c
Vil <vitalik.levchuk.008@gmail.com>
2024-10-13 22:09:31 +0300

    Add enum for classification client threads

--- src/main/java/org/fbs/stgbot/bot/Rule34SearcherBot.java
+++ src/main/java/org/fbs/stgbot/bot/Rule34SearcherBot.java
@@ -7,8 +7,10 @@ import com.pengrad.telegrambot.request.AnswerInlineQuery;
 import com.pengrad.telegrambot.request.SendDocument;
 import org.apache.commons.io.FileUtils;
 import org.fbs.stgbot.bot.base.Bot;
+import org.fbs.stgbot.bot.base.MultiClientBot;
 import org.fbs.stgbot.data.ClientThread;
 import org.fbs.stgbot.data.ClientThreads;
+import org.fbs.stgbot.service.R34SearchService;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,37 +23,19 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Rule34SearcherBot extends Bot {
-
-    private final ArrayList<ClientThreads> parsingThreadsS = new ArrayList<>();
-
+public class Rule34SearcherBot extends MultiClientBot {
 
     public Rule34SearcherBot(String botToken, String startCommandRaw) {
         super(botToken, startCommandRaw);
     }
 
     @Override
-    protected void init() {
+    protected void callbackQueryParse(CallbackQuery query, ArrayList<ClientThreads> threads) throws Exception {
 
     }
 
     @Override
-    protected void onStartCommand(Message message) {
-
-    }
-
-    @Override
-    protected void updateParse(Update update) {
-
-    }
-
-    @Override
-    protected void callbackQueryParse(CallbackQuery query) {
-
-    }
-
-    @Override
-    protected void entitiesParse(MessageEntity[] messageEntities, Message message) {
+    protected void entitiesParse(MessageEntity[] messageEntities, Message message, ArrayList<ClientThreads> threads) throws Exception {
         if (message.text().equals("/sendLog")){
             ClassLoader classloader = ClientThread.currentThread().getContextClassLoader();
             InputStream is = classloader.getResourceAsStream("local/logs/logs.log");
@@ -68,23 +52,8 @@ public class Rule34SearcherBot extends Bot {
     }
 
     @Override
-    protected void messageParse(Message message) {
-
-    }
-
-    @Override
-    protected void inlineQueryParse(InlineQuery query) throws IOException {
-
-        ClientThreads clientThreads = null;
-        for (ClientThreads threads: parsingThreadsS){
-            if (threads.getUserId() == query.from().id()){
-                clientThreads = threads;
-            }
-        }
-        if (clientThreads == null){
-            clientThreads = new ClientThreads(query.from().id());
-            parsingThreadsS.add(clientThreads);
-        }
+    protected void inlineQueryParse(InlineQuery query, ArrayList<ClientThreads> threads) throws Exception {
+        ClientThreads clientThreads = getThreadsByUserId(query.from().id(), threads);
 
         if (Objects.equals(clientThreads.getUserId(), query.from().id())) {
             for (int i = 0; i < clientThreads.size(); i++) {
@@ -99,11 +68,26 @@ public class Rule34SearcherBot extends Bot {
                 LOGGER.error(e);
             }
         });
+    }
+
+    @Override
+    protected void messageParse(Message message, ArrayList<ClientThreads> threads) throws Exception {
 
     }
 
-    private InlineQueryResultPhoto buildInlinePhoto(String id, String url) {
-        return new InlineQueryResultPhoto(id, url, url);
+    @Override
+    protected void onStartCommand(Message message, ArrayList<ClientThreads> threads) throws Exception {
+
+    }
+
+    @Override
+    protected void updateParse(Update update, ArrayList<ClientThreads> threads) throws Exception {
+
+    }
+
+    @Override
+    protected void init() {
+
     }
 
     private InlineQueryResultArticle buildInlineArticle(String id, String title, String message) {
@@ -111,67 +95,16 @@ public class Rule34SearcherBot extends Bot {
     }
 
     private AnswerInlineQuery getRequest(InlineQuery query, String chatId) throws IOException {
-        int pageStr = 0;
-        String pageTag = "";
-        int separatorIndex = query.query().indexOf("-");
-        Document r34Link;
 
-        if (separatorIndex!= -1) {
-            try {
-                pageStr = 42 * (Integer.parseInt(query.query().substring((separatorIndex + 1)).trim()) - 1);
-                if (pageStr <= 0){
-                    pageStr = 0;
-                }
-            }catch (NumberFormatException e){
-                LOGGER.error(e);
-            }
-            pageTag = query.query().substring(0, separatorIndex).trim();
-        }
-        else {
-            pageTag = query.query().trim();
+        InlineQueryResultPhoto[] photos = R34SearchService.getParsed(query.query(), LOGGER);
+
+        LOGGER.trace("Send inline result: {}", (Object) photos);
+        try {
+            return  (new AnswerInlineQuery(query.id(), photos));
+        }catch (RuntimeException e){
+            LOGGER.error(e);
         }
 
-        if (!pageTag.isEmpty()) {
-            LOGGER.trace("Search for rule34 images by tag: {}", pageTag);
-            r34Link = Jsoup.connect("https://rule34.xxx/index.php?page=post&s=list&tags=" + pageTag + "+&pid=" + pageStr).get();
-
-            Elements r34LinkImagesBoard = r34Link.getElementsByClass("image-list");
-            Elements r34RedirectLinks = Jsoup.parse(r34LinkImagesBoard.html()).getElementsByTag("a");
-            ArrayList<String> imgUrls = new ArrayList<>();
-            ArrayList<InlineQueryResultPhoto> photosArray = new ArrayList<>();
-
-            for (Element img : r34RedirectLinks) {
-                imgUrls.add("https://rule34.xxx/" + img.attr("href"));
-            }
-
-            int i = 0;
-            for (String url: imgUrls){
-                LOGGER.trace("Found image: {}", url);
-                try {
-                    Elements flexi = Jsoup.connect(url).get().getElementsByClass("flexi");
-                    Elements img = Jsoup.parse(flexi.html()).getElementsByTag("img");
-                    if (!img.attr("src").isEmpty()) {
-                        photosArray.add(buildInlinePhoto("photo" + i, img.attr("src")));
-                        i++;
-                    }
-                }
-                catch (HttpStatusException e){
-                    LOGGER.error(e.getMessage());
-                }
-            }
-
-            InlineQueryResultPhoto[] photos = new InlineQueryResultPhoto[photosArray.size()];
-            for (int j = 0; j < photosArray.size(); j++) {
-                photos[j] = photosArray.get(j);
-            }
-
-            LOGGER.trace("Send inline result: {}", photosArray);
-            try {
-                return  (new AnswerInlineQuery(query.id(), photos));
-            }catch (RuntimeException e){
-                LOGGER.error(e);
-            }
-        }
         try {
             return new AnswerInlineQuery(chatId, buildInlineArticle("0", "Tag not found", "Tag does not found"));
         }
---
+++ src/main/java/org/fbs/stgbot/data/Action.java
@@ -0,0 +1,12 @@
+package org.fbs.stgbot.data;
+
+public enum Action {
+
+    UPDATE,
+    INLINE_QUERY,
+    CALLBACK_QUERY,
+    MESSAGE,
+    START,
+    ENTITIES
+
+}
